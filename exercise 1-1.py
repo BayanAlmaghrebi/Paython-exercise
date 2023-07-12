9eb03ea417a283266aaf7d765e319ab567289b55
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-07-12 23:47:32 +0200

    add ex. 1

---
+++ exercise 1-1.py
@@ -0,0 +1,16 @@
+'''
+student:
+
+    - creat student (name) -> welcome
+    - add mark
+    - get avg
+
+'''
+
+class Student:
+    def create_student (self,name):
+        print (f'Welcome {name}')
+
+
+s1 = Student()
+s1.create_student('ahmed')
