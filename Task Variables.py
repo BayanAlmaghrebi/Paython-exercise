82f543780c514d1f07e19d23f3b891ea7789fc3e
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-07-14 19:39:34 +0200

    add task variable

---
+++ Task Variables.py
@@ -0,0 +1,79 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+y = 5
+y
+5
+
+x = 'Mahmoud Ali'
+x
+'Mahmoud Ali'
+
+z = 3,4
+z
+(3, 4)
+
+z = 3.4
+z
+3.4
+
+x = 10
+
+bool("x")
+True
+print bool
+SyntaxError: incomplete input
+print (type(x))
+<class 'int'>
+
+y = 5
+y = z
+y
+3.4
+
+
+No we cant turn str to int
+SyntaxError: invalid syntax
+
+l = [1,2,3,4,5]
+l
+[1, 2, 3, 4, 5]
+
+t = (10,11,12,13,14,15)
+t
+(10, 11, 12, 13, 14, 15)
+>>> l = t
+>>> l
+(10, 11, 12, 13, 14, 15)
+>>> print (type(l))
+<class 'tuple'>
+>>> 
+>>> d = {1 : 'Rami' , 2 : 'lama' , 3 : 'Nour'}
+>>> d
+{1: 'Rami', 2: 'lama', 3: 'Nour'}
+>>> 
+>>> 
+>>> Python does not require semi-colons to terminate statements. Semicolons can be used to delimit statements if you wish to put multiple statements on the same line
+SyntaxError: invalid syntax
+>>> 
+>>> Paython is an interpreted language
+SyntaxError: invalid syntax
+>>> 
+>>> Low level:
+...     
+SyntaxError: invalid syntax
+>>> The language of the computer
+... It consists of 1 , 0
+... It is easy to be Executed
+... It is hard to program
+... Can run only of one kind of computer and have to be rewritten to another
+... Like C , Assembly
+... 
+SyntaxError: invalid syntax
+>>> High level:
+...     
+SyntaxError: invalid syntax
+>>> Easy to program
+... Takes less time to be coded
+... Easy to be corrected
+... Portable (only one program can run on any pc)
+... Like java , c# & python
