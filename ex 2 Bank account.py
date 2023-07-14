3545a1c6a8e1b91ae405549f705f2bdc56c783a6
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-07-14 18:29:42 +0200

    saved Bank ex

--- ex 2 Bank account.py
+++ ex 2 Bank account.py
@@ -1,28 +1,61 @@
 Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> 
+
 = RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 2-1 Bank account.py
 Welcome Ali
->>> 
+
 = RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 2-1 Bank account.py
 Welcome Ali
 your current balance : 500
 your current balance : 1500
 your current balance : 700
->>> 
+
 = RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 2-1 Bank account.py
 Welcome Ali
 your current balance : 500
 your current balance : 1500
 your current balance : 700
 your current balance : -300
->>> 
+
 = RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 2-1 Bank account.py
 Welcome Ali
 your current balance : 500
 your current balance : 1500
 insuffecient balance : 1500
 insuffecient balance : 1500
+
+= RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 2-1 Bank account.py
+Welcome Ali
+your current balance : 500
+your current balance : 1500
+your current balance : 700
+insuffecient balance : 700
+
+= RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 2-1 Bank account.py
+Welcome Ali
+your current balance : 500
+your current balance : 1500
+your current balance : 700
+insuffecient balance : 700
+your current balance : -300
+
+= RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 2-1 Bank account.py
+Welcome Ali
+your current balance : 500
+your current balance : 1500
+your current balance : 700
+insuffecient balance : 700
+
+= RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 2-1 Bank account.py
+Welcome Ali
+your current balance : 500
+your current balance : 1500
+your current balance : 700
+insuffecient balance : 700
+Name   : Ali
+Age    : 30
+Gender : Male
+Balance: 700
 >>> 
 = RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 2-1 Bank account.py
 Welcome Ali
@@ -30,3 +63,24 @@ your current balance : 500
 your current balance : 1500
 your current balance : 700
 insuffecient balance : 700
+Name   : Ali
+Age    : 30
+Gender : Male
+Balance: 700
+>>> 
+= RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 2-1 Bank account.py
+Traceback (most recent call last):
+  File "C:/Users/bayan/Desktop/my codes/Paython exercise/ex 2-1 Bank account.py", line 45, in <module>
+    c1 = Bank('Ali',30,'Male')
+TypeError: Bank.__init__() takes 1 positional argument but 4 were given
+>>> 
+= RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 2-1 Bank account.py
+Welcome Ali
+your current balance : 500
+your current balance : 1500
+your current balance : 700
+insuffecient balance : 700
+Name   : Ali
+Age    : 30
+Gender : Male
+Balance: 700
