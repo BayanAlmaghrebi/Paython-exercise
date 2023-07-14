23056246c40678432b5b25dd3e66f67e4d212193
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-07-14 20:56:30 +0200

    add task operators

---
+++ Task Operators.py
@@ -0,0 +1,76 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+x = 10
+x == 10
+True
+x == 11
+False
+
+What do we mean by using !=
+SyntaxError: invalid syntax
+
+x = 10
+x != 11
+True
+
+What is the operator precedence
+SyntaxError: invalid syntax
+
+Create a variable x with value of 10
+SyntaxError: invalid syntax
+x = 10
+x
+10
+y = 5
+x += y
+x
+15
+x =/ y
+SyntaxError: invalid syntax
+x /= y
+x
+3.0
+x = 10
+Divide the x value by 5 using assignment operators
+SyntaxError: invalid syntax
+x = 10
+>>> y = 3
+>>> x /= y
+>>> x
+3.3333333333333335
+>>> x = 50
+>>> y = 10
+>>> x/= y
+>>> x
+5.0
+>>> 
+>>> Multiply the x value by 10 using assignment operator
+SyntaxError: invalid syntax
+>>> x = 5
+>>> x *= 2
+>>> x
+10
+>>> 
+>>> Get module of x on 3 using assignment operators
+SyntaxError: invalid syntax
+>>> x %= 3
+>>> x
+1
+>>> Using python print the module of 11 to 4
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+>>> 11%4
+3
+>>> Print the exponent of 2,3
+SyntaxError: invalid syntax
+>>> 2**3
+8
+>>> Divide 11 by 3 using python division
+SyntaxError: invalid syntax
+>>> 11/3
+3.6666666666666665
+>>> Can we divide 11 by 3 and get an integer number ?
+... Python Operators Tasks
+SyntaxError: invalid syntax
+>>> 11//3
+3
