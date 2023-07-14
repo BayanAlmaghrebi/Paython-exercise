fac1a862fc3080f24fc71c30a6e6121d71edc5ed
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-07-14 16:22:47 +0200

    change file name

--- exercise 1-1 Student list.py
+++ exercise 1-1 Student list.py
@@ -16,8 +16,24 @@ class Student:
     def add_mark(self,mark):
         self.marks.append(mark)
 
+    def get_avg(self):
+        print(self.marks)
+        avg = sum(self.marks)/len(self.marks)
+        print(avg)
+
     
 s1 = Student('ahmed')
+s2 = Student('Ali')
 
 s1.add_mark(50)
+s2.add_mark(40)
+s1.add_mark(30)
+s1.add_mark(80)
+s2.add_mark(90)
+s1.add_mark(40)
+s2.add_mark(20)
+s2.add_mark(80)
 
+
+s1.get_avg()
+s2.get_avg()
