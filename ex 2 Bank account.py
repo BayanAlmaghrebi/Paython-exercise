87014aab8c0fb188bd66a1f8dbb91179fe58c76c
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-07-14 18:27:49 +0200

    add Bank ex

---
+++ ex 2 Bank account.py
@@ -0,0 +1,32 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 2-1 Bank account.py
+Welcome Ali
+>>> 
+= RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 2-1 Bank account.py
+Welcome Ali
+your current balance : 500
+your current balance : 1500
+your current balance : 700
+>>> 
+= RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 2-1 Bank account.py
+Welcome Ali
+your current balance : 500
+your current balance : 1500
+your current balance : 700
+your current balance : -300
+>>> 
+= RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 2-1 Bank account.py
+Welcome Ali
+your current balance : 500
+your current balance : 1500
+insuffecient balance : 1500
+insuffecient balance : 1500
+>>> 
+= RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 2-1 Bank account.py
+Welcome Ali
+your current balance : 500
+your current balance : 1500
+your current balance : 700
+insuffecient balance : 700
