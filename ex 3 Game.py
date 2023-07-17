7fc0a5184a42c7baba32f631b8f41580e4116704
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-07-17 20:23:55 +0200

    add games ex

---
+++ ex 3 Game.py
@@ -0,0 +1,302 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=== RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 3-1 Game.py ===
+
+Welcome To Our Game , Enter Game Number
+    1 : Names With Length
+    2 : Filter starts with
+    3 : From To Multiplication Table
+    4 : to exit
+
+
+=== RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 3-1 Game.py ===
+
+Welcome To Our Game , Enter Game Number
+    1 : Names With Length
+    2 : Filter starts with
+    3 : From To Multiplication Table
+    4 : to exit
+
+Enter Game Number : 1
+----
+d = 'ahmed,ali,hassan,mohamd'
+d.split(',')
+['ahmed', 'ali', 'hassan', 'mohamd']
+
+=== RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 3-1 Game.py ===
+
+Welcome To Our Game , Enter Game Number
+    1 : Names With Length
+    2 : Filter starts with
+    3 : From To Multiplication Table
+    4 : to exit
+
+Enter Game Number : 1
+Enter Names Seperated by , : ahmed,ali,hassan
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+=== RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 3-1 Game.py ===
+
+Welcome To Our Game , Enter Game Number
+    1 : Names With Length
+    2 : Filter starts with
+    3 : From To Multiplication Table
+    4 : to exit
+
+Enter Game Number : 1
+Enter Names Seperated by , : ahmed,ali,hassan
+[5, 3, 6]
+
+=== RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 3-1 Game.py ===
+
+Welcome To Our Game , Enter Game Number
+    1 : Names With Length
+    2 : Filter starts with
+    3 : From To Multiplication Table
+    4 : to exit
+
+Enter Game Number : 2
+Enter Names Seperated by , : ahmed,hassan,mohamed,ali
+Enter Char : a
+['ahmed', 'ali']
+
+=== RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 3-1 Game.py ===
+
+Welcome To Our Game , Enter Game Number
+    1 : Names With Length
+    2 : Filter starts with
+    3 : From To Multiplication Table
+    4 : to exit
+
+Enter Game Number : 3
+Enter Start Number : 5
+Enter End Number : 10
+5 X 1 = 5
+5 X 2 = 10
+5 X 3 = 15
+5 X 4 = 20
+5 X 5 = 25
+5 X 6 = 30
+5 X 7 = 35
+5 X 8 = 40
+5 X 9 = 45
+5 X 10 = 50
+-----------------
+6 X 1 = 6
+6 X 2 = 12
+6 X 3 = 18
+6 X 4 = 24
+6 X 5 = 30
+6 X 6 = 36
+6 X 7 = 42
+6 X 8 = 48
+6 X 9 = 54
+6 X 10 = 60
+-----------------
+7 X 1 = 7
+7 X 2 = 14
+7 X 3 = 21
+7 X 4 = 28
+7 X 5 = 35
+7 X 6 = 42
+7 X 7 = 49
+7 X 8 = 56
+7 X 9 = 63
+7 X 10 = 70
+-----------------
+8 X 1 = 8
+8 X 2 = 16
+8 X 3 = 24
+8 X 4 = 32
+8 X 5 = 40
+8 X 6 = 48
+8 X 7 = 56
+8 X 8 = 64
+8 X 9 = 72
+8 X 10 = 80
+-----------------
+9 X 1 = 9
+9 X 2 = 18
+9 X 3 = 27
+9 X 4 = 36
+9 X 5 = 45
+9 X 6 = 54
+9 X 7 = 63
+9 X 8 = 72
+9 X 9 = 81
+9 X 10 = 90
+-----------------
+10 X 1 = 10
+10 X 2 = 20
+10 X 3 = 30
+10 X 4 = 40
+10 X 5 = 50
+10 X 6 = 60
+10 X 7 = 70
+10 X 8 = 80
+10 X 9 = 90
+10 X 10 = 100
+-----------------
+
+=== RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 3-1 Game.py ===
+
+    Welcome To Our Game , Enter Game Number
+        1 : Names With Length
+        2 : Filter starts with
+        3 : From To Multiplication Table
+        4 : to exit
+    
+Enter Game Number : 1
+Enter Names Seperated by , : ahmed,ali,hassan
+[5, 3, 6]
+
+    Welcome To Our Game , Enter Game Number
+        1 : Names With Length
+        2 : Filter starts with
+        3 : From To Multiplication Table
+        4 : to exit
+    
+Enter Game Number : 3
+Enter Start Number : 5
+Enter End Number : 7
+5 X 1 = 5
+5 X 2 = 10
+5 X 3 = 15
+5 X 4 = 20
+5 X 5 = 25
+5 X 6 = 30
+5 X 7 = 35
+5 X 8 = 40
+5 X 9 = 45
+5 X 10 = 50
+-----------------
+6 X 1 = 6
+6 X 2 = 12
+6 X 3 = 18
+6 X 4 = 24
+6 X 5 = 30
+6 X 6 = 36
+6 X 7 = 42
+6 X 8 = 48
+6 X 9 = 54
+6 X 10 = 60
+-----------------
+7 X 1 = 7
+7 X 2 = 14
+7 X 3 = 21
+7 X 4 = 28
+7 X 5 = 35
+7 X 6 = 42
+7 X 7 = 49
+7 X 8 = 56
+7 X 9 = 63
+7 X 10 = 70
+-----------------
+
+    Welcome To Our Game , Enter Game Number
+        1 : Names With Length
+        2 : Filter starts with
+        3 : From To Multiplication Table
+        4 : to exit
+    
+Enter Game Number : n
+Traceback (most recent call last):
+  File "C:/Users/bayan/Desktop/my codes/Paython exercise/ex 3-1 Game.py", line 65, in <module>
+    g = Game()
+  File "C:/Users/bayan/Desktop/my codes/Paython exercise/ex 3-1 Game.py", line 19, in __init__
+    user_choice = int(input('Enter Game Number : '))
+ValueError: invalid literal for int() with base 10: 'n'
+
+=== RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 3-1 Game.py ===
+
+    Welcome To Our Game , Enter Game Number
+        1 : Names With Length
+        2 : Filter starts with
+        3 : From To Multiplication Table
+        4 : to exit
+    
+Enter Game Number : 3
+Enter Start Number : 5
+Enter End Number : 7
+5 X 1 = 5
+5 X 2 = 10
+5 X 3 = 15
+5 X 4 = 20
+5 X 5 = 25
+5 X 6 = 30
+5 X 7 = 35
+5 X 8 = 40
+5 X 9 = 45
+5 X 10 = 50
+-----------------
+6 X 1 = 6
+6 X 2 = 12
+6 X 3 = 18
+6 X 4 = 24
+6 X 5 = 30
+6 X 6 = 36
+6 X 7 = 42
+6 X 8 = 48
+6 X 9 = 54
+6 X 10 = 60
+-----------------
+7 X 1 = 7
+7 X 2 = 14
+7 X 3 = 21
+7 X 4 = 28
+7 X 5 = 35
+7 X 6 = 42
+7 X 7 = 49
+7 X 8 = 56
+7 X 9 = 63
+7 X 10 = 70
+-----------------
+Press any char to play again , n to exit
+
+    Welcome To Our Game , Enter Game Number
+        1 : Names With Length
+        2 : Filter starts with
+        3 : From To Multiplication Table
+        4 : to exit
+    
+Enter Game Number : n
+Traceback (most recent call last):
+  File "C:/Users/bayan/Desktop/my codes/Paython exercise/ex 3-1 Game.py", line 65, in <module>
+    g = Game()
+  File "C:/Users/bayan/Desktop/my codes/Paython exercise/ex 3-1 Game.py", line 19, in __init__
+    user_choice = int(input('Enter Game Number : '))
+ValueError: invalid literal for int() with base 10: 'n'
+>>> 2
+2
+>>> 
+=== RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 3-1 Game.py ===
+
+    Welcome To Our Game , Enter Game Number
+        1 : Names With Length
+        2 : Filter starts with
+        3 : From To Multiplication Table
+        4 : to exit
+    
+Enter Game Number : q
+Traceback (most recent call last):
+  File "C:/Users/bayan/Desktop/my codes/Paython exercise/ex 3-1 Game.py", line 65, in <module>
+    g = Game()
+  File "C:/Users/bayan/Desktop/my codes/Paython exercise/ex 3-1 Game.py", line 19, in __init__
+    user_choice = int(input('Enter Game Number : '))
+ValueError: invalid literal for int() with base 10: 'q'
+>>> 1
+1
+>>> 
+=== RESTART: C:/Users/bayan/Desktop/my codes/Paython exercise/ex 3-1 Game.py ===
+
+    Welcome To Our Game , Enter Game Number
+        1 : Names With Length
+        2 : Filter starts with
+        3 : From To Multiplication Table
+        4 : to exit
+    
+Enter Game Number : 1
+Enter Names Seperated by , : ahmed,ramy,ali
+[5, 4, 3]
+Press any char to play again , n to exitn
